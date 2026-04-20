c1886fe53f144803b10c1dc589dc7fc135baba15
Imre Nagi <imre.nagi@gojek.com>
2026-04-21 05:50:41 +0700

    Wait for DHCP lease before reporting WiFi IP

--- arduino/clauduino_led/clauduino_led.ino
+++ arduino/clauduino_led/clauduino_led.ino
@@ -10,8 +10,18 @@ static void connectWiFi() {
     delay(3000);
   }
 
+  // WiFi.begin() returns WL_CONNECTED as soon as association succeeds,
+  // but DHCP may still be in progress. Wait (up to ~10s) for a valid IP.
+  IPAddress ip = WiFi.localIP();
+  unsigned long waited = 0;
+  while (ip == IPAddress(0, 0, 0, 0) && waited < 10000) {
+    delay(500);
+    waited += 500;
+    ip = WiFi.localIP();
+  }
+
   Serial.print(F("WiFi: connected, IP="));
-  Serial.println(WiFi.localIP());
+  Serial.println(ip);
 }
 
 void setup() {
