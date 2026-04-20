71d8ea61805ce59408b4aa121f886c9865002cf7
Imre Nagi <imre.nagi@gojek.com>
2026-04-21 05:45:55 +0700

    Connect Arduino to WiFi on boot

--- arduino/clauduino_led/clauduino_led.ino
+++ arduino/clauduino_led/clauduino_led.ino
@@ -1,16 +1,31 @@
+#include <WiFiS3.h>
 #include "arduino_secrets.h"
+
+static void connectWiFi() {
+  Serial.print(F("WiFi: connecting to "));
+  Serial.println(SECRET_WIFI_SSID);
+
+  while (WiFi.begin(SECRET_WIFI_SSID, SECRET_WIFI_PASSWORD) != WL_CONNECTED) {
+    Serial.println(F("WiFi: retry in 3s"));
+    delay(3000);
+  }
+
+  Serial.print(F("WiFi: connected, IP="));
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
   Serial.begin(115200);
-  // Give the native USB Serial a moment on Uno R4 WiFi.
   delay(1500);
   Serial.println(F("clauduino-led: boot"));
+  connectWiFi();
 }
 
 void loop() {
-  static unsigned long last = 0;
-  if (millis() - last >= 2000) {
-    last = millis();
-    Serial.println(F("clauduino-led: alive"));
+  // If we drop WiFi, reconnect.
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println(F("WiFi: lost, reconnecting"));
+    connectWiFi();
   }
+  delay(1000);
 }
