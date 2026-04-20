a230e123b98e5f5e7b5b93116d6d34d93d3e56a2
Imre Nagi <imre.nagi@gojek.com>
2026-04-21 05:53:48 +0700

    Connect Arduino to MQTT broker and log incoming topics

--- arduino/clauduino_led/clauduino_led.ino
+++ arduino/clauduino_led/clauduino_led.ino
@@ -1,5 +1,9 @@
 #include <WiFiS3.h>
+#include <ArduinoMqttClient.h>
 #include "arduino_secrets.h"
+
+static WiFiClient wifiClient;
+static MqttClient mqttClient(wifiClient);
 
 static void connectWiFi() {
   Serial.print(F("WiFi: connecting to "));
@@ -24,18 +28,47 @@ static void connectWiFi() {
   Serial.println(ip);
 }
 
+static void connectMQTT() {
+  Serial.print(F("MQTT: connecting to "));
+  Serial.print(SECRET_MQTT_HOST);
+  Serial.print(':');
+  Serial.println(SECRET_MQTT_PORT);
+
+  while (!mqttClient.connect(SECRET_MQTT_HOST, SECRET_MQTT_PORT)) {
+    Serial.print(F("MQTT: failed, error="));
+    Serial.println(mqttClient.connectError());
+    delay(3000);
+  }
+  Serial.println(F("MQTT: connected"));
+
+  mqttClient.subscribe("clauduino/led/#");
+  Serial.println(F("MQTT: subscribed to clauduino/led/#"));
+}
+
+static void onMqttMessage(int messageSize) {
+  String topic = mqttClient.messageTopic();
+  Serial.print(F("MQTT rx: topic="));
+  Serial.print(topic);
+  Serial.print(F(" size="));
+  Serial.println(messageSize);
+  // Drain payload (we don't use it yet).
+  while (mqttClient.available()) {
+    mqttClient.read();
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   delay(1500);
   Serial.println(F("clauduino-led: boot"));
   connectWiFi();
+
+  mqttClient.onMessage(onMqttMessage);
+  connectMQTT();
 }
 
 void loop() {
-  // If we drop WiFi, reconnect.
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println(F("WiFi: lost, reconnecting"));
-    connectWiFi();
-  }
-  delay(1000);
+  if (WiFi.status() != WL_CONNECTED) { connectWiFi(); }
+  if (!mqttClient.connected())       { connectMQTT(); }
+  mqttClient.poll();
 }
